1ee531910244a106b79f32e3108744249953ddfc
kartik-04 <04kartik04@gmail.com>
2025-05-21 22:46:36 +0530

    Button, popularProductCard, servicesCard, services, superQuality, popularProduct has been added

--- src/components/button.jsx
+++ src/components/button.jsx
@@ -10,12 +10,14 @@ const button = ({ label, iconURL }) => {
     >
       {label}
 
-      <img
-        src={iconURL}
-        alt="arrow right icon"
-        className="ml-2 rounded-full
+      {iconURL && (
+        <img
+          src={iconURL}
+          alt="arrow right icon"
+          className="ml-2 rounded-full
         w-5 h-5 "
-      />
+        />
+      )}
     </button>
   );
 };
