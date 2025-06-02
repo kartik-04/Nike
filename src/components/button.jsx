9aa7afcf637ed4dde44528318bb5509c93a90158
kartik-04 <04kartik04@gmail.com>
2025-06-03 00:36:17 +0530

    Added customerReview and superQuality section

--- src/components/button.jsx
+++ src/components/button.jsx
@@ -1,12 +1,17 @@
-const button = ({ label, iconURL }) => {
+const button = ({
+  label,
+  iconURL,
+  bakcgroundColor,
+  borderColor,
+  textColor,
+}) => {
   return (
     <button
-      className="flex
-    justify-center items-center gap-2
-    px-7 py-4 border font-montserrat
-    text-lg leading-none bg-coral-red
-    text-white rounded-full
-    border-coral-red"
+      className={`flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none ${
+        bakcgroundColor
+          ? `${bakcgroundColor} ${textColor} ${borderColor}`
+          : "bg-coral-red text-white border-coral-red"
+      } rounded-full`}
     >
       {label}
 
